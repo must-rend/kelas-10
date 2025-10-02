1e26f8385b2dca2b7993a37d5b12c4a194300bac
must-rend <olehmang1234@gmail.com>
2025-10-02 18:24:01 +0700

    Add files via upload

---
+++ Kelas 10/Video/PHP/1-6/lat01.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "Belajar PHP";
+
+echo "<br>";
+
+echo "saya siswa SMK";
+
+echo '<h1 style="background-color:red"> Belajar PHP itu mudah</h1>
+    <p style="background-color:yellow"> saya suka belajar program</p>';
+
+echo "<Saya belajar'.'PHP'.'<br>";
+
+echo  2019 + 2;
+?>
